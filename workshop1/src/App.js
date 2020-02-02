8f1db172b27d1d2ec1d7c490fe59230a351c8a2e
aoaae <60562492+aoaae@users.noreply.github.com>
2020-02-02 16:27:15 +0700

    react workshop1

---
+++ workshop1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ทดสอบการทำงานของ React JS</h1>
+      <p></p>
+      <img src="image/npru-logo.png" width="200" height="220" alt=""></img>
+    </div>
+  );
+}
+
+export default App;
